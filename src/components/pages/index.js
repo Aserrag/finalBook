e4fb93cf2eff38a24126169a60f510f471cc7f6f
Aserrag <a.serrag@hotmail.com>
2023-11-14 23:30:15 +0200

    update 1

---
+++ src/components/pages/index.js
@@ -0,0 +1,6 @@
+export { default as Login } from "./login";
+export { default as Home } from "./home";
+export { default as Library } from "./library";
+// export { default as Loader } from "./Loader";
+// export { default as SearchBar } from "./SearchBar";
+// export { default as UserProfile } from "./UserProfile";
