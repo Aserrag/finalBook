0c9db824c10b4fa2cbdc917789255aa8b39b0d84
Motaz Osama <eosamaahmed@student.eelu.edu.eg>
2023-11-15 04:39:08 +0200

    edit

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -4,7 +4,7 @@ import SearchBar from '../items/searchBar'
 
 const Home = () => {
   return (
-    <div className="w-full h-full flex flex-col bg-indigo-900  justify-center text-center" >
+    <div className="w-full h-full flex flex-col justify-center text-center" >
       <Header/>
       <SearchBar/>
     <div>
--- src/components/pages/library.jsx
+++ src/components/pages/library.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Header from '../items/header'
+import SearchBar from '../items/searchBar'
+import Card from './../items/card';
 
 
 const Library = () => {
   return (
     <div>
       <Header/>
+      <SearchBar/>
+      
+
     <div>Library</div>
+    
+    <Card />
     </div>
   )
 }
