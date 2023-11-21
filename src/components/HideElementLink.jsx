ade0b2f8a09813fe51037a470695abdff4997360
Motaz Osama <eosamaahmed@student.eelu.edu.eg>
2023-11-21 22:52:57 +0200

    bookpage

---
+++ src/components/HideElementLink.jsx
@@ -0,0 +1,13 @@
+import React, { useContext, useState } from 'react'
+import { NavLink } from 'react-router-dom'
+// import { ContextApp } from '../ContextAPI'
+
+function HideElementLink(props){
+  const {link, html} = props
+  const {hide} ="Hide";
+// = useContext(ContextApp)
+  return <div className={hide?'hide':''}  style={{cursor: 'auto'}}>
+      {html} 
+  </div>
+}
+export default HideElementLink
